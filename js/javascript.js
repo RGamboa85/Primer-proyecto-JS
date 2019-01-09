90a8edde224fb94303a9fccf46f7316731f42132
Rode Roldan-Gamboa <rgalola1985@gmail.com>
2019-01-08 22:40:24 -0600

    Tratando de hacer estos JSON

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,24 @@
-var estudiantes = [
+var estudiantes1= [
     {
-        "código":"1-0", "nombre":"María del Carmen García", "nota":90
+        "código":"A101", "nombre":"María del Carmen García", "nota":90
     },{
-        "código":"2-0", "nombre":"Juan José Brenes", "nota":98
+        "código":"A201", "nombre":"Juan José Brenes", "nota":98
     },{
-        "código":"3-0", "nombre":"Lidia Mendoza", "nota":80
+        "código":"A301", "nombre":"Lidia Mendoza", "nota":80
     },{
-        "código":"4-0", "nombre":"Santiago Bermudez", "nota":70
+        "código":"A401", "nombre":"Santiago Bermudez", "nota":70
     },{
-        "código":"5-0", "nombre":"Estefanía Nieto", "nota":96
+        "código":"A501", "nombre":"Estefanía Nieto", "nota":96
     },{
-        "código":"6-0", "nombre":"Claudia Aguilar", "nota":75
+        "código":"A601", "nombre":"Claudia Aguilar", "nota":75
     },{
-        "código":"7-0", "nombre":"Milena Torres", "nota":85
+        "código":"A701", "nombre":"Milena Torres", "nota":85
     },{
-        "código":"8-0", "nombre":"José Szchumacher", "nota":65
+        "código":"A801", "nombre":"José Szchumacher", "nota":65
     },{
-        "código":"9-0", "nombre":"Esteban Vega", "nota":73
+        "código":"A901", "nombre":"Esteban Vega", "nota":73
     },{
-        "código":"1-1", "nombre":"Eleonora Martinez", "nota":90
+        "código":"A111", "nombre":"Eleonora Martinez", "nota":90
     }
 ]
 
@@ -26,12 +26,26 @@ function leerJSON(json){
     var out = "----------Estudiantes---------<br>";
     var i;
     for(i=0;i<json.length;i++){
-        out+="Código: "+json[i].codigo+" - "+"Nombre: "+json[i].nombre+" - "+"Nota: "+json[i].nota+"<br>";
+        out+= "Código: "+json[i].codigo+" - "+"Nombre: "+json[i].nombre+" - "+"Nota: "+json[i].nota+"<br>";
     }
     document.getElementById("estudiantes").innerHTML=out;
 }
 
+function notas1(json){
+    var out = "----------Promedio---------<br>";
+    var notas = json[0].nota;
+    var pos = 0;
+    var aux = "";
+    for(i=0;j<json.length;i++){
+        out+= "Notas: "+json[i].nota+"<br>";
+    }
+    document.getElementById("notas").innerHTML="Los promedios son: "+"<br>"+aux;
+}
+
 function estudiantes(){
-    leerJSON(estudiantes);
+    leerJSON(estudiantes1);
+}
+function notas(){
+    notas1(estudiantes1);
 }
 
